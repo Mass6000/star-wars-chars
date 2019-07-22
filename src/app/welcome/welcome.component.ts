c784f0c30cf7e9cf538e73e15ad74909d4dad704
John Chase <john.chase@live.com>
2019-07-22 08:03:13 -0600

    Class Example day 3 complete

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'swc-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
